9bd94651bef8bf80b3fb092b7dad9bece7b6f561
YeXiaoChao <yechao@ycdoit.com>
2016-08-03 14:43:17 +0800

    update

---
+++ js/base.js
@@ -0,0 +1,5 @@
+
+//*********************���� content �ĸ߶�***********************//
+var windowHeight = window.innerHeight;
+document.getElementById("content").style.minHeight = (windowHeight - 99) + "px";
+
